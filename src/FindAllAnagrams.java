78a0796e21611689004552277de232703644d68d
george <abductcows@gmail.com>
2022-12-15 23:28:19 +0200

    5

--- src/FindAllAnagrams.java
+++ src/FindAllAnagrams.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-class SolutionMuchoGood {
+class SolutionMuchogood {
     public List<Integer> findAnagrams(String s, String p) {
 
         final int pL = p.length(), sL = s.length();
@@ -22,15 +22,27 @@ class SolutionMuchoGood {
             ++sFreqs[sChars[i]];
         }
 
+        boolean lastWas = false;
         if (Arrays.equals(pFreqs, 97, 123, sFreqs, 97, 123)) {
             result.add(0);
+            lastWas = true;
         }
         final int limit = sL - pL + 1;
         for (int i = 1; i < limit; ++i) {
-            --sFreqs[sChars[i - 1]];
-            ++sFreqs[sChars[i + pL - 1]];
-            if (Arrays.equals(pFreqs, 97, 123, sFreqs, 97, 123)) {
+            byte c1 = sChars[i - 1];
+            byte c2 = sChars[i + pL - 1];
+            --sFreqs[c1];
+            ++sFreqs[c2];
+
+            if (!lastWas && c1 == c2) {
+                lastWas = false;
+                continue;
+            }
+            if (lastWas && c1 == c2 || Arrays.equals(pFreqs, 97, 123, sFreqs, 97, 123)) {
                 result.add(i);
+                lastWas = true;
+            } else {
+                lastWas = false;
             }
         }
 
