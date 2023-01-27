ae0af7d34d31160f48e78c77129abd5f4f89048c
george <abductcows@gmail.com>
2023-01-27 22:22:47 +0200

    314132414

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,50 +1,44 @@
-class Solution {
+import java.util.*;
 
-    int m, n;
-    IntQueue q = new IntQueue();
+class OrderedStream {
 
-    public int[][] rotateGrid(int[][] grid, int k) {
-        m = grid.length;
-        n = grid[0].length;
+    String[] values;
 
-        for (int i = 0, limit = Math.min(m, n) / 2, div = (m + n - 2) << 1; i < limit; ++i, div -= 8, q.reset()) {
-            rotate(grid, i, i, k % div);
-        }
-
-        return grid;
+    public OrderedStream(int n) {
+        values = new String[n];
     }
 
-    public void rotate(int[][] matrix, int row, int col, int k) {
-        for (int i = col; i < n - col; ++i) q.add(matrix[row][i]);
-        for (int i = row + 1; i < m - row - 1; ++i) q.add(matrix[i][n - col - 1]);
-        for (int i = n - col - 1; i >= col; --i) q.add(matrix[m - row - 1][i]);
-        for (int i = m - row - 1 - 1; i > row; --i) q.add(matrix[i][col]);
-        for (int i = 0; i < k; ++i) q.add(q.poll());
-        for (int i = col; i < n - col; ++i) matrix[row][i] = q.poll();
-        for (int i = row + 1; i < m - row - 1; ++i) matrix[i][n - col - 1] = q.poll();
-        for (int i = n - col - 1; i >= col; --i) matrix[m - row - 1][i] = q.poll();
-        for (int i = m - row - 1 - 1; i > row; --i) matrix[i][col] = q.poll();
+    public List<String> insert(int key, String value) {
+        values[key - 1] = value;
+        return new Haha(key);
     }
 
+    class Haha extends AbstractList<String> implements RandomAccess {
 
-    static class IntQueue {
-        int head = 0, tail = 255;
-        int[] data = new int[256];
+        int start;
+        String[] values;
 
-        void reset() {
-            head = 0;
-            tail = 255;
+        Haha(int start) {
+            this.start = start;
+            this.values = OrderedStream.this.values;
         }
 
-        void add(int e) {
-            tail = tail + 1 & 255;
-            data[tail] = e;
+        public String get(int index) {
+            int i = start;
+            while (index > 0) {
+                if (values[i] != null) {
+                    --index;
+                }
+                ++i;
+            }
+            while (values[i] == null) ++i;
+            return values[i];
         }
 
-        int poll() {
-            int e = data[head];
-            head = head + 1 & 255;
-            return e;
+        public int size() {
+            return (int) Arrays.stream(values, start, values.length)
+                    .filter(Objects::nonNull)
+                    .count();
         }
     }
 }
